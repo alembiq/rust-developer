e4821dc12bc891d179fb6a8d612799c85a41b8d5
Karel Kremel <charles@alembiq.net>
2024-04-25 23:30:29 +0200

    lesson 02

--- lesson02/src/main.rs
+++ lesson02/src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
+use std::io;
+use slug::slugify;
+
+
 fn main() {
-    println!("Hello, world!");
-}
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("Usage: {} lowercase/uppercase/no-space/slugify/reverse/capitalize", args[0]);
+        return;
+    };
+
+
+    println!("What should I transform? ");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let input = input.trim();
+
+
+    let output = match &args[1][..] {
+        "lowercase" => input.to_lowercase(),
+        "uppercase" => input.to_uppercase(),
+        "no-space" => input.replace(" ",""),
+        "slugify" => slugify(input),
+        "reverse" => input.chars().rev().collect(),
+        "capitalize" => format!("{}{}", (&input[..1].to_string()).to_uppercase(), &input[1..]),
+        _ => {
+            println!("Invalid transformation");
+            return;
+        },
+    };
+
+    println!("{}: {}", args[1], output);}
