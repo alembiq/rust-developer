0dc39943f23ed056c85319d3b52fcaf59d98eddf
Karel Kremel <charles@alembiq.net>
2024-06-27 08:54:58 +0200

    migrate 13 to 15

---
+++ lesson15/src/bin/client.rs
@@ -0,0 +1,129 @@
+use std::env;
+use std::fs;
+use std::io;
+use std::net::TcpStream;
+use std::process;
+use std::thread::{self, JoinHandle};
+
+use eyre::{anyhow, bail, Context, Result};
+
+use lesson15::{
+    create_directory, current_time, filename_from_input, image_to_png, incoming_message,
+    outgoing_message, read_file, server_address, MessageType, DIRECTORY_FILES, DIRECTORY_IMAGES,
+};
+
+fn main() -> Result<()> {
+    let server_address: String = server_address(env::args().collect());
+
+    println!(
+        "{} Client connecting to {}!",
+        current_time(),
+        server_address
+    );
+
+    let outgoing_stream = match TcpStream::connect(server_address) {
+        Ok(s) => s,
+        Err(e) => {
+            bail!("Unable to connect: {e}")
+        }
+    };
+
+    let incoming_stream = outgoing_stream
+        .try_clone()
+        .context("TCP stream was already closed")?;
+
+    let outgoing_handle = outgoing(outgoing_stream);
+    let incoming_handle = incoming(incoming_stream);
+
+    outgoing_handle?
+        .join()
+        .map_err(|_| anyhow!("Thread panicked"))
+        .context("Failed to spawn outgoing thread")?
+        .context("Outgoing handle returned error")?;
+
+    incoming_handle
+        .join()
+        .map_err(|_| anyhow!("Failed to spawn incoming thread, or thread panicked"))?;
+
+    Ok(())
+}
+
+fn outgoing(mut stream: TcpStream) -> Result<JoinHandle<Result<()>>> {
+    let help_message = "What to send? (text / .image <filename> / .file <filename> / .quit): ";
+
+    println!("{help_message}");
+
+    let handle = thread::spawn(move || loop {
+        let mut user_input: String = Default::default();
+        io::stdin().read_line(&mut user_input)?;
+        let trimmed_input = user_input.trim();
+        if trimmed_input.is_empty() {
+            println!("{help_message}");
+        } else {
+            let message: MessageType = {
+                match trimmed_input.split_whitespace().next().unwrap_or_default() {
+                    ".quit" => {
+                        println!("{} Exiting!", current_time(),);
+                        process::exit(0)
+                    }
+                    ".file" => MessageType::File(
+                        filename_from_input(trimmed_input)?.to_string(),
+                        read_file(trimmed_input.to_string()),
+                    ),
+                    ".image" => {
+                        MessageType::Image(image_to_png(filename_from_input(trimmed_input)?))
+                    }
+                    _ => MessageType::Text(trimmed_input.to_string()),
+                }
+            };
+            if let Err(e) = outgoing_message(&mut stream, &message) {
+                eprintln!(
+                    "{} Failed to broadcast message: {message:?} -> {e}",
+                    current_time()
+                );
+            }
+        };
+    });
+
+    Ok(handle)
+}
+
+fn incoming(mut stream: TcpStream) -> JoinHandle<()> {
+    create_directory(DIRECTORY_FILES);
+    create_directory(DIRECTORY_IMAGES);
+    thread::spawn(move || loop {
+        let message = match incoming_message(&mut stream) {
+            Ok(res) => res,
+            Err(e) => {
+                eprintln!("{} Stream inter: {e}", current_time());
+                //FIXME infinite loop
+                process::exit(1);
+            }
+        };
+
+        match message {
+            MessageType::Text(text) => {
+                println!("{} {text:?}", current_time());
+            }
+            MessageType::File(name, content) => {
+                //TODO unable to save
+                //TODO file already exist
+                fs::write(format!("{}/{}", DIRECTORY_FILES, name), content)
+                    .expect("Could not write file");
+                println!("{} Receiving {name}", current_time());
+            }
+            MessageType::Image(image) => {
+                //TODO unable to save
+                //TODO file already exist
+                let timestamp: String = std::time::UNIX_EPOCH
+                    .elapsed()
+                    .unwrap()
+                    .as_secs()
+                    .to_string();
+                fs::write(format!("{}/{}.png", DIRECTORY_IMAGES, timestamp), &image)
+                    .expect("Could not write file");
+                println!("{} Receiving {timestamp}.png", current_time());
+            }
+        }
+    })
+}
---
+++ lesson15/src/bin/server.rs
@@ -0,0 +1,92 @@
+use std::collections::HashMap;
+use std::env;
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::sync::Arc;
+use std::thread;
+
+use eyre::{bail, Result};
+use parking_lot::Mutex;
+
+use lesson15::{current_time, incoming_message, outgoing_message, server_address, MessageType};
+
+fn main() -> Result<()> {
+    let server_address: String = server_address(env::args().collect());
+    println!("{} Starting server on {}!", current_time(), server_address);
+    listen_and_accept(server_address)?;
+    Ok(())
+}
+
+fn listen_and_accept(address: String) -> Result<()> {
+    let listener = match TcpListener::bind(address) {
+        Ok(l) => l,
+        Err(e) => {
+            bail!("{} Unable to listen: {e}", current_time())
+        }
+    };
+
+    let clients: Arc<Mutex<HashMap<SocketAddr, TcpStream>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        let addr = stream.peer_addr().unwrap();
+        println!("{} {} stream started", current_time(), addr);
+
+        {
+            clients.lock().insert(addr, stream.try_clone().unwrap());
+        }
+
+        let clients_clone = clients.clone();
+
+        thread::spawn(move || loop {
+            let message = match incoming_message(&mut stream) {
+                Ok(msg) => msg,
+                Err(e) => {
+                    eprintln!("{} {addr} stream interrupted: {e}", current_time());
+                    break;
+                }
+            };
+
+            // message other clients
+            let mut clients_lock = clients_clone.lock();
+            let mut peers_to_remove = vec![];
+            for (peer_addr, peer_stream) in clients_lock.iter_mut() {
+                if *peer_addr == addr {
+                    continue;
+                }
+
+                if let Err(e) = outgoing_message(peer_stream, &message) {
+                    eprintln!(
+                        "{} failed to send message: {message:?} -> {e}",
+                        current_time()
+                    );
+                    peers_to_remove.push(peer_addr.clone());
+                }
+            }
+
+            for peer_addr in peers_to_remove {
+                clients_lock.remove(&peer_addr);
+            }
+
+            drop(clients_lock);
+
+            //MESSAGE SNEAKPEAK
+            match message {
+                MessageType::Text(text) => {
+                    println!("{} {addr}: {text:?}", current_time());
+                }
+                MessageType::File(name, _content) => {
+                    println!("{} {addr} sending: {}", current_time(), name);
+                }
+                MessageType::Image(_image) => {
+                    let timestamp: String = std::time::UNIX_EPOCH
+                        .elapsed()
+                        .unwrap()
+                        .as_secs()
+                        .to_string();
+                    println!("{} {addr} sending: {}.png", current_time(), timestamp);
+                }
+            }
+        });
+    }
+    Ok(())
+}
