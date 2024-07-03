88e686000f8d7e2beba5b94dbe6d0f70ee921c39
Karel Kremel <charles@alembiq.net>
2024-07-03 12:09:59 +0200

    collecting data for prometheus

--- lesson18/src/bin/server18.rs
+++ lesson18/src/bin/server18.rs
@@ -10,13 +10,42 @@ use std::thread;
 
 use eyre::{bail, Result};
 use parking_lot::Mutex;
+// use prometheus::{Gauge, Opts, Registry, Counter};
+use lazy_static::lazy_static;
+use prometheus::{self, register_int_counter, register_int_gauge, Encoder, IntCounter, IntGauge, TextEncoder};
 
 use lesson18::{incoming_message, outgoing_message, server_address, timestamp, MessageType};
+
+lazy_static! {
+    static ref COUNTER_MESSAGE_TEXT: IntCounter =
+        register_int_counter!("message_text", "Text messages delivered").unwrap();
+    static ref COUNTER_MESSAGE_FILE: IntCounter =
+        register_int_counter!("message_file", "Files delivered").unwrap();
+    static ref COUNTER_MESSAGE_IMAGE: IntCounter =
+        register_int_counter!("message_image", "Images delivered").unwrap();
+    static ref COUNTER_CLIENTS: IntCounter =
+        register_int_counter!("clients", "Clients over time").unwrap();
+    static ref GAUGE_CLIENTS: IntGauge =
+        register_int_gauge!("clients_current", "Active clients").unwrap();
+}
 
 fn main() -> Result<()> {
     let server_address: String = server_address(env::args().collect());
     println!("{} Starting server on {}!", timestamp(), server_address);
+
+    // Register & measure some metrics.
+    let mut buffer = Vec::new();
+    let encoder = TextEncoder::new();
+    // Gather the metrics.
+    let metric_families = prometheus::gather();
+    // Encode them to send.
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+    let output = String::from_utf8(buffer.clone()).unwrap();
+    println!("{output:?}");
+
+    // Listen and process clients
     listen_and_accept(server_address)?;
+
     Ok(())
 }
 
@@ -34,6 +63,8 @@ fn listen_and_accept(address: String) -> Result<()> {
         let mut stream = stream.unwrap();
         let addr = stream.peer_addr().unwrap();
         println!("{} {} stream started", timestamp(), addr);
+        GAUGE_CLIENTS.inc();
+        COUNTER_CLIENTS.inc();
 
         {
             clients.lock().insert(addr, stream.try_clone().unwrap());
@@ -46,6 +77,7 @@ fn listen_and_accept(address: String) -> Result<()> {
                 Ok(msg) => msg,
                 Err(e) => {
                     eprintln!("{} {addr} stream interrupted: {e}", timestamp());
+                    GAUGE_CLIENTS.dec();
                     break;
                 }
             };
@@ -74,9 +106,11 @@ fn listen_and_accept(address: String) -> Result<()> {
             match message {
                 MessageType::Text(text) => {
                     println!("{} {addr}: {text:?}", timestamp());
+                    COUNTER_MESSAGE_TEXT.inc();
                 }
                 MessageType::File(name, _content) => {
                     println!("{} {addr} sending: {}", timestamp(), name);
+                    COUNTER_MESSAGE_FILE.inc();
                 }
                 MessageType::Image(_image) => {
                     let filename: String = std::time::UNIX_EPOCH
@@ -85,6 +119,7 @@ fn listen_and_accept(address: String) -> Result<()> {
                         .as_secs()
                         .to_string();
                     println!("{} {addr} sending: {}.png", timestamp(), filename);
+                    COUNTER_MESSAGE_IMAGE.inc();
                 }
             }
         });
